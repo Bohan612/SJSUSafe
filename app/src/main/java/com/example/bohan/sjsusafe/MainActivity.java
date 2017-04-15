3a6032819bd2b9d49077ea6cdd3bf627a9385bcf
bohan <bohan612@gmail.com>
2017-04-14 17:11:04 -0700

    Contact event for alert friends button.

--- app/src/main/java/com/example/bohan/sjsusafe/MainActivity.java
+++ app/src/main/java/com/example/bohan/sjsusafe/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bohan.sjsusafe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        final Button button = (Button) findViewById(R.id.alertFriendBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intent= new Intent(Intent.ACTION_PICK,  ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, 1);
             }
         });
     }
