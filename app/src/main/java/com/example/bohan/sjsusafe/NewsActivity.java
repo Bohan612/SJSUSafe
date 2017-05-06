b3bd6c3fc1c64cce62ee2d2b69e37d8dee16de11
Student <maitry7.shah@gmail.com>
2017-05-05 21:41:57 -0700

    maitri commit

--- app/src/main/java/com/example/bohan/sjsusafe/NewsActivity.java
+++ app/src/main/java/com/example/bohan/sjsusafe/NewsActivity.java
@@ -9,5 +9,7 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        //maitri
     }
 }
